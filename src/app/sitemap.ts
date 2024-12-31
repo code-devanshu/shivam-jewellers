d2ec7158e784f2fb4023106820b07f62c8ad6ba2
code-devanshu <code.devanshu@gmail.com>
2024-12-31 23:05:56 +0530

    updated sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,8 +4,48 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://www.shivamjewellers.co.in/",
-      lastModified: new Date("2024-12-09T16:26:20+00:00"),
+      lastModified: new Date("2024-12-31T17:33:21+00:00"),
       priority: 1.0,
+    },
+    {
+      url: "https://www.shivamjewellers.co.in/products",
+      lastModified: new Date("2024-12-31T17:33:21+00:00"),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.shivamjewellers.co.in/products/tukrey-gold-necklace-18k750?id=cm52ikbpx0002jn031zg38ktd",
+      lastModified: new Date("2024-12-31T17:33:21+00:00"),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.shivamjewellers.co.in/products/tukrey-gold-necklace-18k750?id=cm52ii1ea0001jn03gxk1vsaw",
+      lastModified: new Date("2024-12-31T17:33:21+00:00"),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.shivamjewellers.co.in/products/tukrey-gold-necklace-18k750?id=cm52ifj4o0000jn03wpm954ia",
+      lastModified: new Date("2024-12-31T17:33:21+00:00"),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.shivamjewellers.co.in/products/bracelet?id=cm4wmvjqb0000la031x9lickj",
+      lastModified: new Date("2024-12-31T17:33:21+00:00"),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.shivamjewellers.co.in/products/long-set?id=cm4vff21g0000mt03lfyx2z9k",
+      lastModified: new Date("2024-12-31T17:33:21+00:00"),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.shivamjewellers.co.in/products/earring?id=cm4vbcrm30000l4031whxt61r",
+      lastModified: new Date("2024-12-31T17:33:21+00:00"),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.shivamjewellers.co.in/products/earring?id=cm4v4vjl40000jz03db0nyp02",
+      lastModified: new Date("2024-12-31T17:33:21+00:00"),
+      priority: 0.64,
     },
   ];
 }
